430d5d09e5b92f0f5883b4e14c7b6628648e1a8d
SumitKurkure <sumitkurkure@gmail.com>
2022-04-07 21:12:25 +0530

    ne w..

--- React_Frontend/src/BookAppointment/BookAppointment.js
+++ React_Frontend/src/BookAppointment/BookAppointment.js
@@ -7,6 +7,7 @@ const BookAppointment = () => {
   const navigate = useNavigate();
 
   const[hospList,setHospList]= useState([]);
+
   const [city,setCity]= useState({cityName :""});
   const [flag,setFlag]= useState(false);
 
@@ -16,7 +17,11 @@ const BookAppointment = () => {
       await axios.get("http://localhost:8080/bookappointment/hospital/"+city.cityName).then((Response)=>{
       
           setHospList(Response.data);
+
           setFlag(true);
+
+          console.log(Response.data)
+
             console.log(hospList); // two time clicks get data issue
             // navigate("/bookappointment/showhospital/"+city.cityName);
 
@@ -30,6 +35,65 @@ const BookAppointment = () => {
       var value = e.target.value;
       setCity({...city,[name] : value})  
     }
+
+  return (
+    
+    <div>
+        <div class="card text-center mb-3">      
+        <div class="card-body">
+          <h5 class="card-title mb-2">Search Hospital By City</h5>
+          <input class="mb-2" name='cityName' onChange={onChangeSetCity} size={35} placeholder='Enter the City Name'/>
+          <p></p>
+          <Link to="" class="btn btn-primary mb-3" onClick={showHospital}>Search</Link>
+          <p></p>
+          <Link to={''} className="btn btn-outline-dark me-2 " onClick={showHospital}>Pune</Link>
+          <Link to={''} className="btn btn-outline-dark me-2" onClick={showHospital}>Mumbai</Link>
+          <Link to={''} className="btn btn-outline-dark me-2" onClick={showHospital}>Nagpur</Link>
+          <Link to={''} className="btn btn-outline-dark me-2" onClick={showHospital}>Nashik</Link>
+        </div>
+          <div>
+          <div class="row m-2 ">
+              <div class="col-sm-3">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">Cold,Cough and Fever</h5>
+                    <img class="card-text m-3" src='cough-cold.jpg'  height={135}></img>
+                    <Link to="/bookappointment/showdoctor" class="btn btn-primary">See Doctor</Link>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-3">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title" >Dermatology</h5>
+                    <img class="card-text m-3" src="darmetology.jpg" height={135}></img>
+                    <Link to="/bookappointment/showdoctor" class="btn btn-primary">See Doctor</Link>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-3">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">General Physician</h5>
+                    <img class="card-text m-3" src="physician.webp" height={135}></img>
+                    <Link to="/bookappointment/showdoctor" class="btn btn-primary">See Doctor</Link>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-3">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">Cardiology</h5>
+                    <img class="card-text m-3" src="cardiology.jpg" height={135}></img>
+                    <Link to="/bookappointment/showdoctor" class="btn btn-primary">See Doctor</Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+    </div>
+  )
 
   return (
     
